855dd75d5ba8235768c8dbe92bc1f9ccb7c56ce9
Florian <florian.schmidt.welzow@t-online.de>
2021-05-26 00:06:09 +0200

    Fix method signature of overridden methods

--- Scripts/4_World/4_Hooking/!TerritoryFlag.c
+++ Scripts/4_World/4_Hooking/!TerritoryFlag.c
@@ -1,17 +1,17 @@
 modded class TerritoryFlag {
     ref _Event _registeredInstance = new _Event("TerritoryFlag", "pennant", this);
 
-    override InitRefresherData() {
+    override void InitRefresherData() {
         super.InitRefresherData();
-        if(GetGameLabs()) {
-            if(GetGameLabs().IsServer()) {
+        if (GetGameLabs()) {
+            if (GetGameLabs().IsServer()) {
                 GetGameLabs().RegisterEvent(this._registeredInstance);
             }
         }
     }
 
-    override RemoveRefresherPosition() {
-        super.RemoveRefresherPosition();
+    override void RemoveRefresherPosition(int idx = -2) {
+        super.RemoveRefresherPosition(idx);
         if(GetGameLabs()) {
             if(GetGameLabs().IsServer()) {
                 if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
